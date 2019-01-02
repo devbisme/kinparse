5980edc5a15753346084962a4f471f6a2dd9f802
Roman Valls Guimera <brainstorm@nopcode.org>
2019-01-02 23:13:04 +1100

    Add suitable assertions for the example netlists

--- tests/test_kinparse.py
+++ tests/test_kinparse.py
@@ -4,12 +4,15 @@ from .setup_teardown import *
 
 def test_kinparse_1():
     ntlst = parse_netlist('tests/data/test.net')
+    assert len(ntlst) == 69
     a = ntlst.design.source.val
 
 def test_kinparse_2():
     ntlst = parse_netlist('tests/data/gardenlight.net')
+    assert len(ntlst) == 93
     a = ntlst.design.source.val
 
 def test_kinparse_3():
     ntlst = parse_netlist('tests/data/ref2by2.net')
+    assert len(ntlst) == 13
     a = ntlst.design.source.val
